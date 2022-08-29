be21de8eae187b6720f7095577b1ba7c5b2be83f
jaejin <jaejin94@ajou.ac.kr>
2022-08-30 00:33:17 +0900

    기본 Q-Type 활용

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,68 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        // member1 찾기
+        Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl() {
+        // member1 찾기
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em); // 필드레벨로 올려도 된다. 동시성 이슈 없음.
+//        QMember m = new QMember("m"); // 어떤 QMember인지 구분하기 위해 파라미터를 넣어줘야 함.
+//        QMember m = QMember.member; // 아래처럼 static import로 사용하는 것 권장.
+
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
